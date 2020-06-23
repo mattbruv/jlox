6c9ddefd39ae857dde38e8f3b983356b4609c5dc
Matt <5638426+mattbruv@users.noreply.github.com>
2020-06-23 10:04:31 -0400

    start parser

--- src/com/craftinginterpreters/lox/AstPrinter.java
+++ src/com/craftinginterpreters/lox/AstPrinter.java
@@ -2,17 +2,6 @@ package com.craftinginterpreters.lox;
 
 // Creates an unambiguous, if ugly, string representation of AST nodes.
 class AstPrinter implements Expr.Visitor<String> {
-  public static void main(String[] args) {
-    Expr expression = new Expr.Binary(
-        new Expr.Unary(
-            new Token(TokenType.MINUS, "-", null, 1),
-            new Expr.Literal(123)),
-        new Token(TokenType.STAR, "*", null, 1),
-        new Expr.Grouping(
-            new Expr.Literal(45.67)));
-
-    System.out.println(new AstPrinter().print(expression));
-  }
 
   String print(Expr expr) {
     return expr.accept(this);
